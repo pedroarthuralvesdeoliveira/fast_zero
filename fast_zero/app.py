ca35ea591cdd0a1890cf4a5c93db7b3dd8e97b74
pedroarthuralvesdeoliveira <pedroarthuralvesdeoliveira@hotmail.com>
2024-03-01 21:07:16 -0300

    Implement DELETE and PATCH endpoints for todos

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from fast_zero.routes import auth, users
+from fast_zero.routes import auth, todos, users
 from fast_zero.schemas import Message
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get('/', status_code=200, response_model=Message)
